59d0782b5325111aac49ae44d6c51002069fcaad
TomWeldon <weldontom@eircom.net>
2021-02-09 19:49:04 +0000

    modified code

--- bitcoin.py
+++ bitcoin.py
@@ -22,23 +22,9 @@ dollarRate = bitCoinDict['bpi']['USD']['rate_float']
 euroRate = bitCoinDict['bpi']['EUR']['rate_float']
 poundRate = bitCoinDict['bpi']['GBP']['rate_float']
 
-# input Bitcoin value to convert
-bitCoinToConv = float(input('Please enter the value of Bitcoin to convert: '))
+# printing  current USD, Euro and GBP  rate for 1 bitcoin corrected to two decimal places
 
-# conversion to USD,euro and GBP
-dollars = bitCoinToConv * dollarRate
-euro = bitCoinToConv * euroRate
-pounds = bitCoinToConv * poundRate
-
-
-# printing  USD , Euro and GBP rate for 1 bitcoin corrected to two decimal places
-# print converted Bitcoin value to USD, Euro and GBP corrected to two decimal places
 print('')
 print('1 Bitcoin = ${:.2f} (US Dollars)'.format(dollarRate))
 print('\t    €{:.2f} (Euro)'.format(euroRate))
 print('\t    £{:.2f} (Pound Sterling)'.format(poundRate))
-print('\n ')
-print('A Bitcoin value of {}:'.format(bitCoinToConv))
-print('\t\t\t= ${:.2f} (US Dollars)'.format(dollars))
-print('\t\t\t= €{:.2f} (Euro)'.format(euro))
-print('\t\t\t= £{:.2f} (Pound Sterling)'.format(pounds))
